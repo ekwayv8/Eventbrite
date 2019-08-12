05fffab7c05bab56a1e830da21dd511e95464033
ekwayv8 <yvon.ekwalla@gmail.com>
2019-08-12 23:38:31 +0200

    Ajout des api key au .env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #Lignes permettant d'utiliser la gem Letter_opener
+  #config.action_mailer.delivery_method = :letter_opener 
+
+  #Ligne permettant d'envoyer l'email à son destinataire
+  config.action_mailer.perform_deliveries = true
 end
